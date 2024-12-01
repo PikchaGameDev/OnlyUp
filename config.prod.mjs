fa8774ba000dc6a8ad242f0e2ea48ac0643f086e
PikchaGameDev <LawsAreBull@protonmail.com>
2024-12-02 01:43:04 +0300

    vite config fixes for correct deploy

--- config.prod.mjs
+++ config.prod.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   base: "./",
   logLevel: "warning",
   build: {
+    outDir: path.resolve(__dirname, "docs"),
     rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "./index.html"),
+      },
       output: {
         manualChunks: {
           phaser: ["phaser"],
