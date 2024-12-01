95d3ce23d12184c750e1094d0c41728946e83c77
PikchaGameDev <LawsAreBull@protonmail.com>
2024-12-02 01:36:10 +0300

    Compression for images

--- src/scenes/boot.ts
+++ src/scenes/boot.ts
@@ -8,14 +8,10 @@ export class Boot extends Phaser.Scene {
   preload() {
     this.load.setPath("assets");
 
-    console.log("aaaaaaaaa2");
-
     this.load.image("preloadBackground", "PreloadBackground.png");
   }
 
   create() {
-    console.log("aaaaaaaaa1");
-
     this.scene.start("Preload");
   }
 }
